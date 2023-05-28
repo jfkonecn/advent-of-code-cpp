f242e7f577214c2fa65329b6e94fe574b076d2cd
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-05-28 10:42:50 -0400

    inital commit

---
+++ src/year_2021/day_01/solution.cpp
@@ -0,0 +1,6 @@
+#include <advent_of_code_2021_day_01/solution.h>
+
+int multiply(int a, int b) {
+    return a * b;
+}
+
---
+++ tests/year_2021/day_01_tests.cpp
@@ -0,0 +1,9 @@
+#include <advent_of_code_2021_day_01/solution.h>
+#include <gtest/gtest.h>
+
+TEST(MultiplyTests, TestIntegerOne_One)
+{
+    const auto expected = 1;
+    const auto actual = multiply(1, 1);
+    ASSERT_EQ(expected, actual);
+}
