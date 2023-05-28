0c1fdaa03d9d8989626eb468366a4fa23e1973ab
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-05-28 14:52:45 -0400

    a

--- src/year_2021/day_01/solution.cpp
+++ src/year_2021/day_01/solution.cpp
@@ -1,6 +1,20 @@
 #include <advent_of_code_2021_day_01/solution.h>
+#include <limits.h>
+#include <sstream>
+#include <string>
+#include <vector>
 
 int solution(std::string contents) {
-  // test
-  return 0;
+  std::stringstream ss(contents);
+  std::string line;
+  int count = 0;
+  auto last = INT_MAX;
+  while (std::getline(ss, line, '\n')) {
+    auto current = std::stoi(line);
+    if (current > last) {
+      count++;
+    }
+    last = current;
+  }
+  return count;
 }
--- tests/year_2021/test_day_01.cpp
+++ tests/year_2021/test_day_01.cpp
@@ -2,9 +2,16 @@
 #include <advent_of_code_2021_day_01/solution.h>
 #include <gtest/gtest.h>
 
-TEST(Test_2021_Day_01, Example) {
+TEST(Test_2021_Day_01, Example_1) {
   auto str = get_file_contents("example.txt");
   const auto expected = 7;
+  const auto actual = solution(str);
+  ASSERT_EQ(expected, actual);
+}
+
+TEST(Test_2021_Day_01, REAL_1) {
+  auto str = get_file_contents("real.txt");
+  const auto expected = 1681;
   const auto actual = solution(str);
   ASSERT_EQ(expected, actual);
 }
